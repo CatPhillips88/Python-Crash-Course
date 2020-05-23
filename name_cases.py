9bfbb89bbecaf5d8540b61a6ade4b63cb99b7447
Catherine <catherineep4189@gmail.com>
2020-05-23 13:41:35 +0100

    string formatting using cases

---
+++ name_cases.py
@@ -0,0 +1,20 @@
+# Exercises
+
+# Personal Message: Use a variable to represent a person's name, and print a message to that person. 
+# Your message should be simple, such as, "Hello Eric, would you like to learn Python today?"
+
+name = "Cat"
+
+print(f"Hello {name}, would you like to learn Python today?")
+
+# Name Cases: Use a variable to represent a person's name, and then print that person's name in 
+# lowercase, uppercase, and title case.
+
+name = "jennifer oliver"
+
+print(f"{name.lower()}")
+print(f"{name.upper()}")
+print(f"{name.title()}")
+
+
+  
