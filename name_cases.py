39f879f5374bed5056631525170ff40ba8aea0d3
Catherine <catherineep4189@gmail.com>
2020-05-23 14:37:35 +0100

    adding white space using tabs and newlines then using stripping functions

--- name_cases.py
+++ name_cases.py
@@ -25,3 +25,32 @@ quote = "Infection: Avoid the unhappy or the unlucky"
 author = "robert greene"
 
 print(f'{author.title()} once said, "{quote}".')
+
+# Famous Quote 2: Repeat the previous exercise, but this time represent the famous person's name using
+# a variable called famous_person. Then compose your message and represent it with a new variable called message. 
+# Print your message
+
+famous_person = "robert greene"
+
+message = f'{famous_person.title()}, once said "Infection: Avoid the unhappy or the unlucky".'
+
+print(message)
+
+# Stripping Names: Use a variable to represent a person's name, and include some whitespace characters at the beginning,
+# and end of the name. Make sure you use each character combination, "\t" and "\n", at least once.
+# Print the name once, so the whitespace around the name is display. Then print the name using each of the three stripping
+# functions, lstrip(), rstrip() and strip()
+
+who_are_you = "\tCatherine\n\t "
+
+print(who_are_you)
+
+print(who_are_you.lstrip())
+
+print(who_are_you.rstrip())
+
+print(who_are_you.strip())
+
+
+
+
