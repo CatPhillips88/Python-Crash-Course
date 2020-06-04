60b586e90b3f6714b58a92f0f652f6cec40be0a8
Catherine <catherineep4189@gmail.com>
2020-06-04 16:56:34 +0100

    modifying lists exercise 1

---
+++ changing_lists.py
@@ -0,0 +1,17 @@
+# Exercises
+
+# Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes 
+# at least three people you'd like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+guest_list =  ['Rihanna', 'Steve Jobs', 'Bernardine Evaristo', 'Gucci Mane', 'Gabriel Garcia Marquez']
+
+print(f"You're invite to the cookout, {guest_list[0]}!")
+
+print(f"You're invite to the cookout, {guest_list[1]}!")
+
+print(f"You're invite to the cookout, {guest_list[2]}!")
+
+print(f"You're invite to the cookout, {guest_list[3]}!")
+
+print(f"You're invite to the cookout, {guest_list[4]}!")
+
--- intro-to-lists.py
+++ intro-to-lists.py
@@ -1,3 +1,5 @@
+# Exercises
+
 # Names: Store the names of a few of your friends in a list called names. Print each person's
 # name by accessing each element in the list, one at a time.
 
