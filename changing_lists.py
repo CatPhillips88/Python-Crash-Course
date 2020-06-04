dab0cf22a68053066d8ade8327af2c3db3281405
Catherine <catherineep4189@gmail.com>
2020-06-04 19:48:42 +0100

    modifying lists exercise 2

--- changing_lists.py
+++ changing_lists.py
@@ -5,6 +5,34 @@
 
 guest_list =  ['Rihanna', 'Steve Jobs', 'Bernardine Evaristo', 'Gucci Mane', 'Gabriel Garcia Marquez']
 
+# print(f"You're invite to the cookout, {guest_list[0]}!")
+
+# print(f"You're invite to the cookout, {guest_list[1]}!")
+
+# print(f"You're invite to the cookout, {guest_list[2]}!")
+
+# print(f"You're invite to the cookout, {guest_list[3]}!")
+
+# print(f"You're invite to the cookout, {guest_list[4]}!")
+
+
+# Changing Guest List: You just heard that one of your guests can't make the dinner, so you need to send out a new set of 
+# invitations. You'll have to think of someone else to invite.
+
+# Start with your program from the previous exercise. Add a print() call at the end of your program stating the name of
+# the guest who can't make it.
+
+# Modify your list, replacing the name of the guest who can't make it with the name of the new person you are inviting.
+
+# Print a second set of invitation messages, one for each person who is still in your list.
+
+cancelled_guest = 'Gucci Mane'
+guest_list.remove(cancelled_guest)
+
+print(f"Unfortunately {cancelled_guest} cannot make it! You'll be missed!")
+
+guest_list.insert(3, 'Robert Greene')
+
 print(f"You're invite to the cookout, {guest_list[0]}!")
 
 print(f"You're invite to the cookout, {guest_list[1]}!")
@@ -14,4 +42,5 @@ print(f"You're invite to the cookout, {guest_list[2]}!")
 print(f"You're invite to the cookout, {guest_list[3]}!")
 
 print(f"You're invite to the cookout, {guest_list[4]}!")
+
 
