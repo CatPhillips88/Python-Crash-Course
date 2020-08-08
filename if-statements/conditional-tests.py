d5eb2b7aaccad8822bd093c688d4819278c49aa3
Catherine <catherineep4189@gmail.com>
2020-08-08 20:55:35 +0100

    additional changes to if statements

--- if-statements/conditional-tests.py
+++ if-statements/conditional-tests.py
@@ -7,9 +7,9 @@
 milkshake = 'Banana'
 
 print(milkshake == 'Banana')
-  
+
 print(milkshake != 'Oreo')
-   
+
 print(milkshake == 'Strawberry')
 
 print(milkshake != 'Banana')
@@ -19,9 +19,9 @@ print(milkshake != 'Banana')
 bridge = 'Tower Bridge'
 
 print(bridge.lower() == 'tower bridge')
- 
+
 print(bridge.lower() == 'TOWER BRIDGE')
- 
+
 # NUMERICAL TESTS - EQUALITY & INEQUALITY, GREATER THAN AND LESS THAN, GREATER THAN OR EQUAL TO, LESS THAN OR EQUAL TO
 
 print(456 == 456)
@@ -34,13 +34,13 @@ print(32 != 32)
 
 print(131261 > 12987)
 
-print (2 < 90)
+print(2 < 90)
 
 print(401 > 555)
 
 print(810 < 809)
 
-print (2.6756 >= 1.45)
+print(2.6756 >= 1.45)
 
 print(765.88 >= 885.67)
 
@@ -51,30 +51,34 @@ print(702 <= 1)
 # TESTS USING THE 'AND' KEYWORD & 'OR' KEYWORD
 
 age_entry = 20
- 
+
 if age_entry >= 18 and age_entry <= 25:
-   print(f'YOU MAY ENTER THE COMPETITION')
+    print('YOU MAY ENTER THE COMPETITION')
+else:
+    print('SORRY, YOU CANNOT ENTER THE COMPETITION THIS TIME')
 
 
 age_entry2 = 16
 
 if age_entry2 >= 18 and age_entry2 <= 25:
-   print(f'YOU MAY ENTER THE COMPETITION')
+    print('YOU MAY ENTER THE COMPETITION')
+else:
+    print('SORRY, YOU CANNOT ENTER THE COMPETITION THIS TIME')
 
 
-user1 = 'cITrine0654'   
+user1 = 'cITrine0654'
 user2 = 'AMethYST327'
 
 if (user1 == 'cITrine0654') or (user2 == 'TouRMALine1245'):
-    print(f'ACCESS GRANTED')
+    print('ACCESS GRANTED')
 else:
-	print(f'ACCESS DENIED')
+    print('ACCESS DENIED')
 
 
 if (user1 == 'QuArtZ129') or (user2 == 'TouRMALine1245'):
-	print(f'ACCESS GRANTED')
+    print('ACCESS GRANTED')
 else:
-	print(f'ACCESS DENIED')
+    print('ACCESS DENIED')
 
 # TEST WHETHER ITEM IS IN A LIST
 
@@ -89,4 +93,3 @@ German_Flag = ['Black', 'Red', 'Yellow']
 
 if 'Blue' not in German_Flag:
     print('YOU HAVE THE WRONG FLAG!')
-
