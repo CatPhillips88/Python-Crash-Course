71b4496ea31953e378e890516fb2a9e6f0ca3334
Catherine <catherineep4189@gmail.com>
2020-08-16 16:27:50 +0100

    further if statement practice

--- if-statements/if-statement-exercises3.py
+++ if-statements/if-statement-exercises3.py
@@ -16,3 +16,17 @@ for username in usernames:
         print('Hello admin, would you like to see a status report?')
     else:
         print(f'Hello {username.title()}, thanks your for logging in again')
+
+
+# No Users: Add an if test to hello admin to make sure the list of users is not empty.
+
+# If the list is empty, print the message We need to find some users!
+
+# Remove all of the usernames from your list, and make sure the correct message is printed.
+
+usernames2 = []
+
+if usernames2:
+    print('We have some users')
+else:
+    print('We need to find some users!')
