2d94614cb410e19696130a26016964a4bbf23fe1
Catherine <catherineep4189@gmail.com>
2020-08-27 19:16:31 +0100

    printing values via key

---
+++ dictionaries/dictionaries_basics.py
@@ -0,0 +1,17 @@
+# Exercises
+
+# Persons: Use a dictionary to store information about a person you know. Store their first name, 
+# last name, and the city which they live. You should have keys such as first_name, last_name, age
+# and city. Print each piece of information stored in your dictionary.
+
+person = {
+    'first_name': 'Jennifer',
+    'last_name': 'Oliver',
+    'age' : 58,
+    'city': 'Kent'
+}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
