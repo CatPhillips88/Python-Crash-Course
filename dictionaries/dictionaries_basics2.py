cc844098c628b40e2011d58d8a436bd173f3e5f7
Catherine <catherineep4189@gmail.com>
2020-09-21 19:41:29 +0100

    looping through key-value pairs in a dictionary

--- dictionaries/dictionaries_basics2.py
+++ dictionaries/dictionaries_basics2.py
@@ -20,4 +20,30 @@ programming_glossary =  {
 for term, description in programming_glossary.items():
     print(f'{term.title()} - {description}')
 
- 
+# Rivers: Make a dictionary containing three major rivers and the country each river runs through. 
+
+# Use a loop to print a sentence about each river, such as The Nile runs through Egypt
+
+# Use a loop to print the name of each river included in the dictionary.
+
+# Use a loop to print the name of each country included in the dictionary.
+
+rivers = {
+    'nile': 'africa',
+    'amazon': 'south america',
+    'yangtze': 'china'
+}
+
+for river, country in rivers.items():
+    if river == 'nile':
+        print(f'The {river.title()} in {country.title()} is the longest river in the world')
+    elif river == 'amazon':
+        print(f'The {river.title()} in {country.title()} is the second longest river in the world')
+    elif river == 'yangtze':
+        print(f'The {river.title()} in {country.title()} is the third longest river in the world')
+
+for river in rivers.keys():
+    print(river.title())
+
+for river in rivers.values():
+    print(river.title())
