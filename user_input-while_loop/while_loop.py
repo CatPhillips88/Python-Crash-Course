b08bd3687e33f6156ccd5fb3c3405878cc002f13
Catherine <catherineep4189@icloud.com>
2020-11-23 16:55:02 +0000

    variations of exiting a while loop

--- user_input-while_loop/while_loop.py
+++ user_input-while_loop/while_loop.py
@@ -10,37 +10,83 @@
 # AS TOPPINGS ARE ADDED TO THE LIST, A FOR LOOP IS USED TO PRINT EACH VALUE.
 # UPON 'QUIT' BEING ENTERED AS A RESPONSE, LOOP IS EXITED - ENDING THE PROGRAM.
 
-topping = ""
+# topping = ""
 
-toppings = []
+# toppings = []
 
-while topping != 'quit':
-    topping = input("What toppings would you like on your pizza? When finished enter 'quit'. ")
-    toppings.append(topping) 
+# while topping != 'quit':
+#     topping = input("What toppings would you like on your pizza? When finished enter 'quit'. ")
+#     toppings.append(topping) 
 
-    if topping != 'quit':
-        print('YOUR PIZZA MASTERPIECE WITH THE FOLLOWING TOPPINGS:')
-        for ingredient in toppings:
-            print(ingredient)
+#     if topping != 'quit':
+#         print('YOUR PIZZA MASTERPIECE WITH THE FOLLOWING TOPPINGS:')
+#         for ingredient in toppings:
+#             print(ingredient)
 
 
 # Movie Tickets: A movie theater charges different ticket prices depending on a person's age. If a person is under the age of 3,
 # the ticket is free. If they are between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is $15. Write a loop
 # in which you ask users their age, and then tell them the cost of their movie ticket.
 
+# while True:
+#     age = int(input('How old are you? '))
+#     if age < 3:
+#         print('Movie tickets are free')
+#         break
+#     elif age < 12:
+#         print('Movie tickets are $10')
+#         break
+#     elif age > 12:
+#         print('Movie tickets are $15')
+#         break
+
+
+# Three Exits: Write different versions of the Pizza Toppings exercise that do each of the following at least once:
+# Use a conditional test in the while statement to stop the loop
+# Use an active variable to control how long the loop runs
+# Use a break statement to exit the loop when the user enters a 'quit' value
+
+# EXAMPLE ONE
+
+topping = ''
+toppings = []
+
+active = True
+
+while len(toppings) < 5:
+    topping = input('What toppings would you like on your pizza? ')
+    toppings.append(topping)
+
+    print('\nYOUR PIZZA  MASTERPIECE INCLUDES THE FOLLOWING TOPPINGS:')
+
+    for ingredient in toppings:
+        print(ingredient)
+
+# # EXAMPLE TWO
+
+while active:
+    topping = input('What toppings would you like on your pizza? ')
+    toppings.append(topping)
+
+    if len(toppings) == 5:
+        print('\nYOUR PIZZA  MASTERPIECE INCLUDES THE FOLLOWING TOPPINGS:')
+
+        for ingredient in toppings:
+            print(ingredient)
+        active = False
+
+# EXAMPLE THREE
+# IF STATEMENT USED WITHIN FOR LOOP TO REMOVE THE VALUE 'quit' FROM THE LIST.
+
 while True:
-    age = int(input('How old are you? '))
-    if age < 3:
-        print('Movie tickets are free')
+    topping = input('What toppings would you like on yur pizza? Enter "quit" when you have finished. ')
+    toppings.append(topping)
+
+    if topping == 'quit':
+        print('\nYOUR PIZZA  MASTERPIECE INCLUDES THE FOLLOWING TOPPINGS:')
+        for ingredient in toppings:
+            if ingredient == 'quit':
+                toppings.remove('quit')
+            else:
+                print(ingredient)
         break
-    elif age < 12:
-        print('Movie tickets are $10')
-        break
-    elif age > 12:
-        print('Movie tickets are $15')
-        break
-
-   
-
-
-
