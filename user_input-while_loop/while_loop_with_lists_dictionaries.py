52d65bc081e2a927ac3d7dbf16c9fad3e98376dd
Catherine <catherineep4189@icloud.com>
2021-02-07 13:27:59 +0000

    further example of using while loop with lists and dictionaries

--- user_input-while_loop/while_loop_with_lists_dictionaries.py
+++ user_input-while_loop/while_loop_with_lists_dictionaries.py
@@ -9,7 +9,7 @@ finished_sandwiches = []
 
 while sandwich_orders:
     order = sandwich_orders.pop()
-    print(f'You\'re {order.title()} sandwich is currently being made')
+    print(f'Your {order.title()} sandwich is currently being made')
     finished_sandwiches.append(order)
 
 
@@ -19,4 +19,32 @@ for sandwich_made in finished_sandwiches:
 print('\nLIST OF FINISHED SANDWICHES')
 for sandwich in finished_sandwiches:
     print(sandwich.title())
+
+# No Pastrami: Using the list sandwich_orders, make sure the sandwich 'pastrami' appears in the list at least three tiimes. Add code near the
+# beginning of your program to print a message saying the deli has run out of pastrami, and then use a while loop to remove all occurences of 
+# 'pastrami' from sandwich_orders. Make sure no pastrami sandwiches end up in finished_sandwiches.
+
+sandwich_orders = ['pastrami', 'grilled cheese', 'pulled pork', 'pastrami', 'club', 'bacon', 'french dip', 'pastrami']
+
+finished_sandwiches = []
+
+print('THE DELI HAS RUN OUT OF PASTRAMI SANDWICHES')
+
+while 'pastrami' in sandwich_orders:
+    remove_sandwich = sandwich_orders.remove('pastrami')
+
+while sandwich_orders:
+    order = sandwich_orders.pop()
+    print(f'Your {order.title()} sandwich is currently being made')
+    finished_sandwiches.append(order)
+
+
+for sandwich_made in finished_sandwiches:
+    print(f'I made your {sandwich_made.title()} sandwich')
+
+print('\nLIST OF FINISHED SANDWICHES')
+for sandwich in finished_sandwiches:
+    print(sandwich.title())
+
+
 
