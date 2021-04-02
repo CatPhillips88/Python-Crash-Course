4307ef4045c8cf171b47a543754112ca9007b06c
Catherine <catherineep4189@icloud.com>
2021-04-02 20:54:04 +0100

    additional print statement

--- functions/functions_exercises1.py
+++ functions/functions_exercises1.py
@@ -85,6 +85,7 @@ while True:
 
    
     artist_profile = make_album2(a_name, a_title, num_songs)
+    print(artist_profile)
 
 for album_features, album_info in artist_profile.items():
     if album_features == 'album tracks':
