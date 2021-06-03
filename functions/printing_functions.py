028b2d84a016d2261361d74a5f68ac4766559085
Catherine <catherineep4189@icloud.com>
2021-06-03 18:54:37 +0100

    import functions as alias

--- functions/printing_functions.py
+++ functions/printing_functions.py
@@ -1,9 +1,9 @@
-from modules import engagement_ring_purchases, shipped_rings
+from modules import engagement_ring_purchases as eg, shipped_rings as sr
 
 ring_orders = ['canary yellow asscher', 'aquamarine pear', 'ruby heart', 'pink tourmaline oval']
 
 completed_orders = []
 
-engagement_ring_purchases(ring_orders, completed_orders)
+eg(ring_orders, completed_orders)
 
-shipped_rings(completed_orders)
+sr(completed_orders)
